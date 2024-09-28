faaa043a83381cadff2e33a11a74f0a6a5ba76bc
Colin Gelling <22787309+colingelling@users.noreply.github.com>
2024-09-28 15:01:09 +0200

    Added more structure; classes for different sections using the CRUD terminology and keeping subjects separated

--- main.py
+++ main.py
@@ -5,27 +5,82 @@
 
 """
 
-from PyQt6 import uic
+from PyQt6 import uic, QtWidgets
+from PyQt6.QtCore import QObject
 from PyQt6.QtGui import QFontDatabase
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication
 
 
-class MainWindow(QMainWindow):
+class DataModel:
+    def __init__(self):
+        super().__init__()
+
+
+class Events(QObject):
+    def __init__(self):
+        super().__init__()
+
+
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
+        # Load QtDesigner Ui file
         qt_creator_file = "src/ui/Overview.ui"
-        uic.loadUi(qt_creator_file, self)
+        self.ui = uic.loadUi(qt_creator_file, self)
 
         self.setWindowTitle("Overview")
 
+        # Load Ui stylesheet
         stylesheet_file_path = "src/css/overview.css"
         with open(stylesheet_file_path, "r") as file:
             stylesheet = file.read()
             self.setStyleSheet(stylesheet)
 
+        # Load FontAwesome font icons
         font_file = "src/fonts/FontAwesome6-Free-Regular-400.otf"
         QFontDatabase.addApplicationFont(font_file)
+
+        self.content()
+
+    def content(self):
+        ui = self.ui
+
+        # Set size properties for the window
+        ui.setMaximumWidth(773)
+        ui.setMinimumHeight(434)
+
+        ui.notebookManagerTitleLabel.setText("My notebooks")
+        ui.notebookManagerTitleLabel.adjustSize()
+
+        ui.contentHeaderWidgetLabel.setText("Welcome")
+        ui.contentHeaderWidgetLabel.adjustSize()
+
+class Dialogs(QtWidgets.QDialog):
+    def __init__(self):
+        super(Dialogs, self).__init__()
+
+    def create_notebook(self):
+        pass
+
+    def create_note(self):
+        pass
+
+    def read_note(self):
+        pass
+
+    def update_notebook(self):
+        pass
+
+    def update_note(self):
+        pass
+
+    def delete_notebook(self):
+        pass
+
+    def delete_note(self):
+        pass
+
 
 if __name__ == "__main__":
     import sys
