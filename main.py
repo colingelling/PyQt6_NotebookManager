ac68e0abf3270ee8c34ead9c73836dedaa5b55c7
Colin Gelling <22787309+colingelling@users.noreply.github.com>
2024-10-08 19:17:43 +0200

    Removed code, improved TreeWidget setup and worked on properties for the context menu -action functionality

--- main.py
+++ main.py
@@ -23,6 +23,13 @@ class DataModel:
         super().__init__()
         self._write_file_template()
 
+    @staticmethod
+    def prepare_context_data(item):
+        parent_item = item.parent()
+
+        if parent_item: return {'parent_notebook': parent_item.text(0), 'child_note': item.text(0)}
+        else: return {'notebook': item.text(0)}
+
     def _write_file_template(self):
         # Declare an empty dictionary containing preset keys representing lists
         preset = {"Notebooks": {}}
@@ -82,58 +89,29 @@ class Events(QObject):
     notebook_edit_signal = pyqtSignal(dict)
     top_level_item_clicked = pyqtSignal(str)  # Signal to send item text
 
-    notebook_form_state = None
-    note_form_state = None
-
-    def __init__(self, widget):
-        super(Events, self).__init__()
-        self.tree_widget = widget
-
-        self.tree_event_data = None
-
-    def _set_filtered_item_data(self, item):
-        parent_item = item.parent()
-
-        if parent_item:
-            self.tree_event_data = {
-                'parent_notebook': parent_item.text(0),
-                'child_note': item.text(0)
-            }
-
-        else:
-            self.tree_event_data = {
-                'notebook': item.text(0),
-            }
-
-    def mousePressEvent(self, event):
-        item = self.tree_widget.itemAt(event.pos())  # Locate the item
-        self._set_filtered_item_data(item)  # The setup of a set containing item data based on the event
-
-        index = self.tree_widget.indexAt(event.pos())
-        tree_item_name = index.data(Qt.ItemDataRole.DisplayRole)
-
-        # TODO: Updating the QlineEdit (QTreeWidget) is not possible because of the new MainWindow instance
-        main_window = MainWindow()
-        main_window.tree_event_data = self.tree_event_data
-        if item: main_window.context_menu_func(event, main_window.edit_notebook_obj, tree_item_name, self.tree_event_data)  # Trigger the context menu if the item is there
-
-        # Return to normal behavior
-        QTreeWidget.mousePressEvent(self.tree_widget, event)
-
-    def getFilteredChildNoteData(self):
-        if 'child_note' in self.tree_event_data:
-            return self.tree_event_data
-
 
 class Widgets:
     tree_widget = None
-    item_data_map = {}
 
     def __init__(self):
         super(Widgets, self).__init__()
+        self.item_data_map = {}
 
-    @staticmethod
-    def TreeWidgetItems(widget):
+    def setup_tree_widget(self, parent_window, model, widget):
+
+        Widgets.tree_widget = widget
+
+        # QTreeWidget customisation
+        widget.setHeaderHidden(True)
+
+        # Execute functionality
+        self.add_tree_items(widget)
+
+        # Extending to context menu display
+        widget.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+        widget.customContextMenuRequested.connect(self.context_menu)
+
+    def add_tree_items(self, widget):
         # Readability reference declaration
         data_elements = DataModel.load_data()
 
@@ -142,69 +120,61 @@ class Widgets:
             notebook_item = QTreeWidgetItem([notebook_name])
             widget.addTopLevelItem(notebook_item)
 
-            Widgets.item_data_map[id(notebook_item)] = notebook_name
+            # Storing object data in order to keep the positions for comparable situations later
+            self.item_data_map[id(notebook_item)] = notebook_name
 
             for note_element in data_elements["Notebooks"][notebook_name]:
                 note_item = QTreeWidgetItem([note_element])
                 notebook_item.addChild(note_item)
 
-                Widgets.item_data_map[id(note_item)] = note_element
+                self.item_data_map[id(note_item)] = note_element
 
-    def context_menu(self, position, item_data):
-        # Get the item at the clicked position
+    def refresh_tree_widget(self, widget):
+        widget.clear()  # Remove all items from the widget
+        self.add_tree_items(widget)  # Rebuild widget items
+
+    @staticmethod
+    def context_menu(position):
+        # Receive the item's clicked position
         selected_item = Widgets.tree_widget.itemAt(position)
 
         if selected_item:
             # Create a context menu
             menu = QMenu()
 
-            # Add 'Show' option
-            show_action = menu.addAction('Show')
+            # Adding options
+            edit_action = menu.addAction('Edit')
+            delete_action = menu.addAction('Delete')
 
-            # Connect 'Show' action to a function
-            show_action.triggered.connect(lambda: self.show_item(selected_item))
+            selected_action = Widgets.tree_widget.viewport().mapToGlobal(position)
 
-            # Display the context menu at the mouse position
+            # Connecting the options to functions
+            edit_action.triggered.connect(lambda: Widgets.activate_trigger(selected_item, selected_action))
+            delete_action.triggered.connect(lambda: Widgets.activate_trigger(selected_item, selected_action))
+
+            # Display the context menu at the mouse position and get the selected action
             menu.exec(Widgets.tree_widget.viewport().mapToGlobal(position))
-        # if event.button() == Qt.MouseButton.RightButton:
-        #
-        #     menu = QMenu()
-        #
-        #     delete_action = QAction("Delete")
-        #     edit_action = QAction("Edit")
-        #
-        #     menu.setCursor(Qt.CursorShape.PointingHandCursor)
-        #
-        #     # TODO: Include both buttons for notebooks and notes
-        #
-        #     # edit_action.triggered.connect(MainWindow.view_note_obj)  # TODO: Forward to DataModel.update_x
-        #     # delete_action.triggered.connect(lambda: verify_action(item_name, collected_item_data))  # TODO: Forward to DataModel.delete_x
-        #
-        #     # The open_note functionality already has the ability to edit
-        #     if 'child_note' not in collected_item_data:
-        #         edit_action.triggered.connect(lambda: obj(collected_item_data))
-        #         menu.addAction(edit_action)
-        #
-        #     # open contextmenu
-        #     menu.addAction(delete_action)
-        #
-        #     menu.exec(event.globalPosition().toPoint())
 
-    def show_item(self, item):
-        if item.parent() is None:
-            # emit signal that the item has been clicked
-            print(item, " has been pressed")
-            pass
+    @staticmethod
+    def activate_trigger(item, action):
+        filtered_data = DataModel.prepare_context_data(item)
+
+        # emit signal that the item has been clicked
+        item_text = item.text(0)
+        filtered_data["pressed_item"] = item_text
+        print("Action ", action, "on", item_text, "has been pressed. ", "The following data set was prepared: ", filtered_data)
 
 
 class MainWindow(QtWidgets.QMainWindow, Widgets, DataModel):
     context_menu_func = None
 
-    edit_notebook_obj = None
-    view_note_obj = None
-
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
+
+        # One time model declaration, need the same signals for passing through functions
+        self.data_model = DataModel()
+        self.widgets_model = Widgets()
+        self.events_model = Events()
 
         # Load QtDesigner Ui file
         qt_creator_file = "src/ui/Overview.ui"
@@ -229,8 +199,6 @@ class MainWindow(QtWidgets.QMainWindow, Widgets, DataModel):
         font_file = "src/fonts/FontAwesome6-Free-Regular-400.otf"
         QFontDatabase.addApplicationFont(font_file)
 
-        self.tree_widget_data = None
-
         self.content()
 
     def content(self):
@@ -241,98 +209,19 @@ class MainWindow(QtWidgets.QMainWindow, Widgets, DataModel):
         ui.notebookManagerTitleLabel.setText("Management of my notebooks")
         ui.notebookManagerTitleLabel.adjustSize()
 
-        tree_widget = ui.QtreeWidget
+        # Widget assignment
+        parent_tree_widget = ui.QtreeWidget
 
-        # QTreeWidget customisation
-        tree_widget.setHeaderHidden(True)
-
-        # Execute functionality
-        Widgets.TreeWidgetItems(tree_widget)
-
-        def refresh_tree_widget():
-            tree_widget.clear()  # Remove all items from the widget
-            Widgets.TreeWidgetItems(tree_widget)  # Rebuild widget items
-
-        # One time model declaration, need the same signals for passing through functions
-        events_model = Events(tree_widget)
+        # TreeWidget setup
+        self.widgets_model.setup_tree_widget(self, self.events_model, parent_tree_widget)
 
         # Set a connection from the view passed signal and connect it with functionality in order to rebuild QTreeWidget items
-        events_model.data_signal.connect(refresh_tree_widget)
-
-        Widgets.tree_widget = tree_widget
-
-        # # Enable mouse tracking and override method for right-clicking support
-        # tree_widget.setMouseTracking(True)
-        # tree_widget.mousePressEvent = events_model.mousePressEvent
-
-        tree_widget.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
-        tree_widget.customContextMenuRequested.connect(Widgets.context_menu)
-
-        # self.context_menu_func = Widgets.context_menu
-
-        # # TODO: open_note process
-        # tree_widget.clicked.connect(
-        #     lambda: read_note(events_model, events_model.getFilteredChildNoteData())
-        #     if events_model.getFilteredChildNoteData() else None
-        # )
-
-        if not events_model.notebook_form_state:
-            events_model.notebook_form_state = "notebook creation mode"
-
-        def add_notebook():
-            # Serve window properties as being the default mode
-            notebook_name = ui.notebookAction_lineEdit.text()
-            print(f"Create notebook using '{notebook_name}'")
-            DataModel.create_notebook(notebook_name)
-
-            # Send signal that the view side could be updated
-            events_model.data_signal.emit()
-
-        def edit_notebook(item_data):
-            # events_model.notebook_form_state = "notebook editing mode"
-            # print(events_model.notebook_form_state)
-            # notebook_name = ui.notebookAction_lineEdit.text()
-
-            print(f"notebook '{item_data['notebook']}' has been pressed - item_data: '{item_data}'")
-
-            events_model.notebook_form_state = "notebook editing mode"
-            events_model.notebook_edit_signal.emit(item_data)
-
-            ui.notebookAction_lineEdit.connect(handle_notebook_input)
-
-            print(f"Editing notebook '{item_data['notebook']}'")
-            # DataModel.update_notebook(notebook_name)
-
-            # Send signal that the view side could be updated
-            events_model.data_signal.emit()
-
-        def handle_notebook_input(item_data):
-            notebook_text = item_data.get('notebook', '')  # Safely access the notebook value
-            print(f"handle_notebook_input: {notebook_text}")
-
-            if notebook_text:
-                ui.notebookAction_lineEdit.setText(notebook_text)  # Update the QLineEdit
-                ui.notebookAction_lineEdit.repaint()  # Force update
-
-        events_model.notebook_edit_signal.connect(handle_notebook_input)
-
-        self.edit_notebook_obj = edit_notebook
-
-        def check_state(value):
-            print("State: ", value, '\n')
-            if "creation mode" in value and "notebook" in value:
-                print('Creation mode active')
-                add_notebook()
-            # elif "editing mode" in value and "notebook" in value:
-            #     # Signal has been set, continue to Edit mode
-            #     print("Edit mode active")
-            #     edit_notebook()
+        self.events_model.data_signal.connect(lambda: self.widgets_model.refresh_tree_widget(parent_tree_widget))
 
         button_text = "Save"
         ui.notebookActionButton.setText(button_text)
         ui.noteActionButton.setText(button_text)
 
-        ui.notebookActionButton.clicked.connect(lambda: check_state(events_model.notebook_form_state))
         ui.notebookActionButton.setCursor(Qt.CursorShape.PointingHandCursor)
 
 
